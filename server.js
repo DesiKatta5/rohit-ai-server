8bff78d7faf293cc9dd7c22a7087ded33138cb73
Rohit Gaming <gbnodes1@gmail.com>
2026-06-30 15:14:07 +0530

    mobile layout fixing

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 const groq = new Groq({
 apiKey: process.env.GROQ_API_KEY
